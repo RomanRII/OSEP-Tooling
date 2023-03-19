443aeba726c0ba57aa7078c105b9b5f2ad6a5928
Roman Rivas II <74742067+RomanRII@users.noreply.github.com>
2023-03-19 06:23:06 -0700

    Update sqlQuery.cs

--- sqlQuery.cs
+++ sqlQuery.cs
@@ -1,45 +1,54 @@
 using System;
-using System.Collections;
 using System.Data.SqlClient;
 
 namespace SQLQuery
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        static SqlConnection authenticate(string sqlServer)
         {
-            // Grabs server you want to auth to. If you press enter without supplying any server, it will try to auth to the current host
-            Console.WriteLine("Enter the server you want to auth to: ");
-            string sqlServer = Console.ReadLine();
-            // Connection info
-            string database = "master";
-            string conString = "Server = " + sqlServer + "; Database = " + database + "; Integrated Security = True;";
+            string conString = String.Format("Server = {0}; Database = master; Integrated Security = True;", sqlServer);
             SqlConnection con = new SqlConnection(conString);
             try
             {
                 con.Open();
-                Console.WriteLine("Auth Success!");
+                Console.WriteLine("[+] Auth Success!");
             }
             catch
             {
                 con.Close();
-                Console.WriteLine("Auth failed");
+                Console.WriteLine("[-] Auth Failed.");
                 Environment.Exit(0);
             }
-            while (1 == 1)
+            return con;
+        }
+
+        static void executeQuery(SqlConnection con, string queryStr)
+        {
+            SqlCommand command = new SqlCommand(queryStr, con);
+            try
             {
-                Console.WriteLine("SQL >");
-                // Enter the command you will like to run
-                string execCmd = Console.ReadLine();
-                SqlCommand command = new SqlCommand(execCmd, con);
                 SqlDataReader reader = command.ExecuteReader();
                 while (reader.Read() == true)
                 {
-                    Console.WriteLine("Output:" + reader[0]);
+                    Console.WriteLine("[+] " + reader[0]);
                 }
-
                 reader.Close();
+                con.Close();
             }
+            catch
+            {
+                con.Close();
+                Console.WriteLine("[-] Command Execution Failed.");
+                Environment.Exit(0);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            SqlConnection con = authenticate(args[0]);
+            executeQuery(con, args[1]);
 
         }
     }
